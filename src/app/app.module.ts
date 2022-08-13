1da12bae2a50eec97a83d9c9d1dd24006320232f
Bekir PİRALP <bpir33@gmail.com>
2022-08-13 19:12:29 +0300

    boostrap eklendi ve ufak güzelleştirme yapıldı

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent, ValueComponent],
-  imports: [BrowserModule,HttpClientModule],
+  imports: [BrowserModule,HttpClientModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
 })
